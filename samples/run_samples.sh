14ca74da865aba18f51049308daf792c040069f3
Vlad Lipskiy <vlad.lipskiy@apryse.com>
2025-05-14 17:35:58 +0300

    Update wording in run_samples.sh

--- samples/run_samples.sh
+++ samples/run_samples.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 find "$SCRIPT_DIR/sandbox" -type f -name '*.py' ! -name '_*' -exec sh -c                  \
-    'echo "Processing $(basename "$(dirname "$1")")/$(basename "$1")..." && python "$1"'  \
+    'echo "Running $(basename "$(dirname "$1")")/$(basename "$1")..." && python "$1"'  \
     find-sh '{}' \;
